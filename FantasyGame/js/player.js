0fa4fe35f3dcf617203c0e66179054055d0e22da
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-06 12:45:37 +0000

    Player manager

---
+++ FantasyGame/js/player.js
@@ -0,0 +1,8 @@
+function Player() {
+    this.classType = classType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
