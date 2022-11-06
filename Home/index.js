71d8e2dabaf5b2cac69390390962175304d5de33
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-11-06 12:46:21 +0000

    Index Page

---
+++ Home/index.js
@@ -0,0 +1,3 @@
+function RegisterWindow() {
+    window.open('C:/Users/User.DESKTOP-8GPHIN0/Documents/コーネリアス・ヴァンダービルトCornelius Vanderbilt/ライダーハミルトン Raidā Hamiruton HTML/project/Register/register.html');
+}
